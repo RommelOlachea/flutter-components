7f7c9a834ea37d9a5e545e1f52b8a16f1b8621ac
Rommel Olachea <msc.rommel.olachea@gmail.com>
2022-10-13 21:45:21 -0700

    Se agrego el cuadro de dialogo

--- lib/screens/alert_screen.dart
+++ lib/screens/alert_screen.dart
@@ -3,8 +3,35 @@ import 'package:flutter/material.dart';
 class AlertScreen extends StatelessWidget {
   const AlertScreen({Key? key}) : super(key: key);
 
-  void displayDialog() {
-    print('Hola mundo2222');
+  void displayDialog(BuildContext context) {
+    showDialog(
+        barrierDismissible: true,
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            elevation: 5,
+            title: const Text('Titulo de la alerta'),
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadiusDirectional.circular(10)),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: const [
+                Text('Este es el contenido de la alerta'),
+                SizedBox(
+                  height: 10,
+                ),
+                FlutterLogo(
+                  size: 100,
+                )
+              ],
+            ),
+            actions: [
+              TextButton(
+                  onPressed: () => Navigator.pop(context),
+                  child: const Text('Cancelar'))
+            ],
+          );
+        });
   }
 
   @override
@@ -18,7 +45,7 @@ class AlertScreen extends StatelessWidget {
           padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
           child: Text('Mostrar Alerta', style: TextStyle(fontSize: 16)),
         ),
-        onPressed: displayDialog,
+        onPressed: () => displayDialog(context),
       )),
       floatingActionButton: FloatingActionButton(
           child: const Icon(Icons.close),
