0c584ed112cd3ae02fd79f5cb979cc16edda627b
unknown <msc.rommel.olachea@gmail.com>
2022-10-14 14:49:28 -0600

    Circle Avatar

---
+++ lib/screens/avatar_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class AvatarScreen extends StatelessWidget {
+  const AvatarScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('STAN LEE'),
+        actions: [
+          Container(
+              margin: const EdgeInsets.only(right: 5),
+              child: CircleAvatar(
+                  backgroundColor: Colors.indigo.shade900,
+                  child: const Text('SL')))
+        ],
+      ),
+      body: const Center(
+          child: CircleAvatar(
+        maxRadius: 100,
+        backgroundImage:
+            NetworkImage('https://i.blogs.es/85aa44/stan-lee/840_560.jpg'),
+      )),
+    );
+  }
+}
