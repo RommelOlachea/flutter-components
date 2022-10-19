b168fa0b514c338661c6b81c0858c4b7d22840c7
unknown <msc.rommel.olachea@gmail.com>
2022-10-19 13:54:48 -0600

    CheckBox y Switch

--- lib/screens/slider_screen.dart
+++ lib/screens/slider_screen.dart
@@ -10,33 +10,63 @@ class SliderScreen extends StatefulWidget {
 
 class _SliderScreenState extends State<SliderScreen> {
   double _sliderValue = 100;
-
+  bool _sliderEnabled = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Sliders && checks'),
       ),
-      body: SingleChildScrollView(
-        child: Column(children: [
-          Slider.adaptive(
-              min: 100,
-              max: 600,
-              activeColor: AppTheme.primary,
-              value: _sliderValue,
-              onChanged: (value) {
-                _sliderValue = value;
-                print(value);
-                setState(() {});
-              }),
-          Image(
-            image: const NetworkImage(
-                'https://www.dlf.pt/dfpng/middlepng/262-2629475_batman-new-52-png-batman-the-dark-knight.png'),
-            fit: BoxFit.contain,
-            width: _sliderValue,
-          )
-        ]),
-      ),
+      body: Column(children: [
+        Slider.adaptive(
+            min: 100,
+            max: 600,
+            activeColor: AppTheme.primary,
+            value: _sliderValue,
+            onChanged: _sliderEnabled
+                ? (value) {
+                    _sliderValue = value;
+                    print(value);
+                    setState(() {});
+                  }
+                : null),
+        Checkbox(
+            value: _sliderEnabled,
+            onChanged: (value) {
+              _sliderEnabled = value ?? true;
+              setState(() {});
+            }),
+        CheckboxListTile(
+            activeColor: AppTheme.primary,
+            title: const Text('Habilitar Slider'),
+            value: _sliderEnabled,
+            onChanged: (value) => setState(() {
+                  _sliderEnabled = value ?? true;
+                })),
+        Switch(
+            value: _sliderEnabled,
+            onChanged: (value) => setState(() {
+                  _sliderEnabled = value ?? true;
+                })),
+        SwitchListTile.adaptive(
+            activeColor: AppTheme.primary,
+            title: const Text('Habilitar Slider'),
+            value: _sliderEnabled,
+            onChanged: (value) => setState(() {
+                  _sliderEnabled = value ?? true;
+                })),
+        const AboutListTile(),
+        Expanded(
+          child: SingleChildScrollView(
+            child: Image(
+              image: const NetworkImage(
+                  'https://www.dlf.pt/dfpng/middlepng/262-2629475_batman-new-52-png-batman-the-dark-knight.png'),
+              fit: BoxFit.contain,
+              width: _sliderValue,
+            ),
+          ),
+        )
+      ]),
     );
   }
 }
